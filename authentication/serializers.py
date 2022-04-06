bbbc419827dce397c1d508713404387f618545a6
RafsanRifat <rafsan.rifat255@gmail.com>
2022-04-06 13:48:16 +0600

    registration view complete

---
+++ authentication/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import User
+
+
+class RegisterApiSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(max_length=50, min_length=6, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+        def create(self, validated_data):
+            return User.objects.create_user(self, **validated_data)
+
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import response
+from rest_framework.generics import GenericAPIView
+from .serializers import RegisterApiSerializer
+from rest_framework.response import Response
+
 
 # Create your views here.
+
+class RegisterApiView(GenericAPIView):
+
+    serializer_class = RegisterApiSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+
+
+
+
